69ea78df20219f2108c2e7d2162fba04cecc240c
nasr <naiw991uen@gmail.com>
2026-08-24 17:17:41 +0300

    Add project files.

---
+++ EF Core 03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EF_Core_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
